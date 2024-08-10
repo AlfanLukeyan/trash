f0234b27710ff45b2f490c2c039945cd0d46c68e
Alfan Lukeyan Rizki <alfanlukeyanrizki@gmail.com>
2024-08-10 15:29:19 +0700

    feat: add suitmedia-test1

---
+++ index.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <title>PenPasBan (Pendaftaran Pass Bandara)</title>
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand js-scroll-trigger" href="#page-top">PenPasBan</a>
+                <button class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="fas fa-bars"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#pengumuman">Pengumuman</a></li>
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#panduan">Panduan</a></li>
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="login.php">Login</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Masthead-->
+        <header class="masthead bg-primary text-white text-center">
+            <div class="container d-flex align-items-center flex-column">
+                <!-- Masthead Avatar Image-->
+                <img class="masthead-avatar mb-5" src="assets/img/avataaars.svg" alt="" />
+                <!-- Masthead Heading-->
+                <h1 class="masthead-heading text-uppercase mb-0">Pendaftaran Pas Bandara</h1>
+                <!-- Icon Divider-->
+                <div class="divider-custom divider-light">
+                    <div class="divider-custom-line"></div>
+                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                    <div class="divider-custom-line"></div>
+                </div>
+                <!-- Masthead Subheading-->
+                <p class="masthead-subheading font-weight-light mb-0">Otoritas Bandar Udara Wilayah II</p>
+            </div>
+        </header>
+        <!-- Portfolio Section-->
+        <section class="page-section portfolio" id="pengumuman">
+            <div class="container">
+                <!-- Portfolio Section Heading-->
+                <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Pengumuman</h2>
+                <!-- Icon Divider-->
+                <div class="divider-custom">
+                    <div class="divider-custom-line"></div>
+                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                    <div class="divider-custom-line"></div>
+                </div>
+    <body>
+                           <section> <p>Kartu Pas Bandara adalah tanda izin masuk daerah terbatas pada area Bandar Udara. Kartu Pas diterbitkan oleh Kantor Otoritas Wilayah pada masing-masing Bandara. Dalam hal ini untuk Bandara Soekarno Hatta adalah Kantor Otoritas Wilayah I.</p></section> 
+
+<p><strong>1.NPA (Non Public Area)</strong></p>
+<p>- Apron/Platform adalah suatu daerah atau tempat yang telah ditentukan guna menempatkan pesawat udara, menurunkan dan menaikkan penumpang, kargo, pos, pengisian bahan bakar dan parkir.<br>
+- Fasilitas Vital yaitu Gedung Tower, Gedung Operasi Lalu Lintas Penerbangan, Gedung Radar, Gedung Listrik, Gedung Pemancar dan Penerima, Gedung PKP-PK, Gedung Meteorologi, Landasan, Taxi Way dan Peralatan Penunjang Navigasi Penerbangan<br>
+- Gudang Kargo yaitu Gudang Kargo Domestik dan Internasional<br>
+- Ruang tunggu keberangkatan, daerah imigrasi, daerah pabean, daerah karantina<br>
+- Daerah kedatangan internasional</p>
+
+<p>&nbsp;</p>
+
+<p><strong>2.RPA (Restrected Public Area)</strong></p>
+<p>Daerah Terbatas adalah bagian dari area bandar udara yang ditetapkan bukan sebagai daerah umum (NPA / Non Public Area) dan area bandar udara yang ditetapkan sebagai daerah umum terbatas (RPA / Restricted Public Area).</p>
+<p>- Daerah Check In<br>
+- Pelataran parkir Gudang Kargo Domestik dan Internasional<br>
+- Gedung Catering dan DPPU Pertamina</p>
+
+<p>Setiap kartu pas yang diterbitkan mempunyai kode yang menunjukkan area kerja dan masa berlaku kartu pas tersebut.Masa berlaku kartu PAS adalah minggun,bulanan,3 bulanan dan tahunan.Setiap habis masa berlakunya,maka kartu PAS bisa diperpanjang lagi.Pemohon kartu PAS pemula biasanya hanya diterbitkan kartu pas dengan masa berlaku yang pendek.Setelah masa kartu PAS berakhir baru diperbolehkan untuk mengajukan permohonan kartu pas dengan masa berlaku lebih panjang.Dari kartu pas mingguan,kemudian bulanan,3 bulanan dan setelah berakhir kartu pas 3 bulanan baru diperbolehkan untuk mengajukan permohonan kartu pas tahunan.</p>
+
+<p>Area dimana kartu pas itu bisa digunakan direpresentasikan dalam kode-kode.Kode area dalam kartu PAS menunjukkan dimana si pemegang kartu PAS boleh masuk di sebuah area tertentu dan dimana si pemegang kartu PAS dilarang masuk.<br>
+Kode area dalam kartu PAS adalah sebagai berikut :<br>
+<br>
+P = Platform<br>
+A = Arrival Hall<br>
+B = Boarding Lounge<br>
+C = Chek In Counter<br>
+S = Shopping Arcade<br>
+T = Tower<br>
+G = Gudang ( bagian dalam )<br>
+F = Gudang ( bagian luar / halaman gudang )<br>
+Ain = Arrival International<br>
+V = Proyek Vital</p>
+
+<p>&nbsp;</p>
+
+<p>Persyaratan pembuatan kartu PAS Bandara :<br>
+1.Kontrak Kerja ( dasar hukum pemohon untuk masuk ke wilayah bandara )<br>
+2.Surat permohonan dari atasan tertinggi<br>
+3.Surat Keterangan catatan Kepolisian ( SKCK )<br>
+4.Surat Perjanjian Kerja Waktu tertentu ( PKWT ) bagi karyawan kontrak,surat penetapan karyawan bagi karyawan tetap.<br>
+5.Daftar Riwayat Hidup<br>
+6.Surat Pernyataan<br>
+7.ID Card atau Surat Pengganti ID Card<br>
+8. Foto Kopi KTP<br>
+<br>
+Setelah syarat-syarat dilengkapi dan permohonan diajukan, pihak Otoritas Bandara akan mengadakan screening bagi pemohon kartu PAS Bandara.</p>
+                                
+                                <h4>Gallery &amp; Files</h4>
+                                <div class="row">
+                                                                                                            <div class="col-md-3 mt-30">
+                                        <div class="box-hover-effect">
+                                          <div class="effect-wrapper">
+                                            <div class="thumb">
+                                              <img class="img-fullwidth" style="height: 120px; object-fit: cover;" src="https://otban-wil1.dephub.go.id/resource/doc/post/210430130927contoh_pass.jpg">
+                                            </div>
+                                            <div class="overlay-shade"></div>
+
+                                            <div class="icons-holder icons-holder-middle">
+                                              <div class="icons-holder-inner">
+                                                <div class="styled-icons icon-sm icon-dark icon-circled icon-theme-colored">
+                                                  <a href="https://otban-wil1.dephub.go.id/resource/doc/post/210430130927contoh_pass.jpg" data-lightbox-gallery="gallery1"><i class="fa fa-picture-o"></i></a>
+                                                </div>
+                                              </div>
+                                            </div>
+                                          </div>
+                                        </div>
+                                    </div>
+        <!-- About Section-->
+        <section class="page-section bg-primary text-white mb-0" id="panduan">
+            <div class="container">
+                <!-- About Section Heading-->
+                <h2 class="page-section-heading text-center text-uppercase text-white">Panduan Pendaftaran</h2>
+                <!-- Icon Divider-->
+                <div class="divider-custom divider-light">
+                    <div class="divider-custom-line"></div>
+                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                    <div class="divider-custom-line"></div>
+                </div>
+                <!-- About Section Content-->
+                <div class="row">
+                    <div class="col-lg-4 ml-auto"><p class="lead">Pendaftaran untuk pembuatan Pas Bandara dapat dilakukan dengan melengkapi Persyaratan yang telah terlampir.</p></div>
+                    <div class="col-lg-4 mr-auto"><p class="lead">Langkah pertama mendaftar adalah klik tombol sign in, jika belum memiliki akun, harap sign up terlebih dahulu. Kemudian lengkapi data dan berkas yang diminta.</p></div>
+                </div>
+                <!-- About Section Button-->
+                <div class="text-center mt-4">
+                    <a class="btn btn-xl btn-outline-light" href="login.php">
+                        Daftar
+                    </a>
+                </div>
+            </div>
+        </section>
+</body>
+
+
+        <!-- Footer-->
+        <footer class="footer text-center">
+            <div class="container">
+                <div class="row">
+                    <!-- Footer Location-->
+                    <div class="col-lg-4 mb-5 mb-lg-0">
+                        <h4 class="text-uppercase mb-4">Telepon</h4>
+                        <p class="lead mb-0">
+                            0777-8678-09045
+                        </p>
+                    </div>
+                    <!-- Footer Social Icons-->
+                    <div class="col-lg-4 mb-5 mb-lg-0">
+                        <h4 class="text-uppercase mb-4">Media Sosial</h4>
+                        <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-facebook-f"></i></a>
+                        <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-twitter"></i></a>
+                        <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-linkedin-in"></i></a>
+                        <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-dribbble"></i></a>
+                    </div>
+                    <!-- Footer About Text-->
+                    <div class="col-lg-4">
+                        <h4 class="text-uppercase mb-4">Alamat Kantor</h4>
+                        <p class="lead mb-0">
+                            JL. Kayu Batu No. 6 Tanjung Ria, Jayapura Utara, Jayapura
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- Copyright Section-->
+        <div class="copyright py-4 text-center text-white">
+            <div class="container"><small>Pendaftaran Pas Bandara - Otoritas Bandar Udara Wilayah II</small></div>
+        </div>
+        <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes)-->
+        
+       
+    </body>
+</html>
